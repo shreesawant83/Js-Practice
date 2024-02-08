2463bbdb36ec69de798aa548287f03c55cffd0f5
shreesawant83 <81132129+shreesawant83@users.noreply.github.com>
2024-02-08 17:52:16 +0000

    learn about comparison

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,15 @@
+//Comparison
+// console.log("1">0);  //true
+// console.log("01">0);  //true
+
+// console.log(null > 0);  //false 
+// console.log(null == 0);  //false
+// console.log(null >= 0); //true
+//Above example null is converting as o and then compare.
+
+//=== Comparison
+// console.log("1" === 1);  //true
+// console.log(1 === 1);  //true
+
+//Its compare value plus data type
+
