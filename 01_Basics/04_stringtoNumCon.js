7d6d49d46e1b5e742b3e74b0be9787440c8a6bf1
shreesawant83 <81132129+shreesawant83@users.noreply.github.com>
2024-02-08 17:40:52 +0000

    stribng to num comparison

---
+++ 01_Basics/04_stringtoNumCon.js
@@ -0,0 +1,14 @@
+//Operations
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log("1"+2+"2");
+console.log(1+2+"2");
+//depending upon starting value calculate value
+
+console.log(+true);
+//gives 1 value but try to write clean code
+
+//console.log(true+);
+//trow error
