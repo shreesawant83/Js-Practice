71778463edeb3d368fbc649f8e8cb1fd63623040
shreesawant83 <81132129+shreesawant83@users.noreply.github.com>
2024-02-10 13:40:09 +0000

    learn about arrays and some methods

---
+++ 02_Basics/array.js
@@ -0,0 +1,36 @@
+//covered Arrays
+//Declaration
+let myArrayNumber=[1,2,3,4,5,6];
+// console.log(myArrayNumber);
+
+// let myArrayString = ["Shreepad","Sushant"];
+// console.log(myArrayString);
+
+// let myArrayNumString= new Array(1,2,"Shreeepad",4,5)
+// console.log(myArrayNumString);
+
+
+//Array Methods
+// myArrayNumber.push(7);
+// myArrayNumber.pop()
+// myArrayNumber.unshift(10)
+// myArrayNumber.shift()
+// console.log(myArrayNumber.includes(2));
+// console.log(myArrayNumber.indexOf(2));
+// console.log(myArrayNumber.join()); //convert string in comma seperate
+// console.log("A",myArrayNumber);
+
+// let Array1= myArrayNumber.slice(0,3);
+// console.log(Array1);
+
+// console.log("B",myArrayNumber);
+
+// let Array2 =myArrayNumber.splice(0,3);
+// console.log(Array2);
+
+// console.log("C",myArrayNumber);
+
+
+//Slice method did not manipulate your original array.
+//Splice method did manipulate your array 
+
