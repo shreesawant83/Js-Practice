bfd6f7a052ca72c00e7ed2cc1264c9b7bbf12d1d
shreesawant83 <81132129+shreesawant83@users.noreply.github.com>
2024-02-11 09:24:08 +0000

    learn about api , json and objects

---
+++ 02_Basics/05objects.js
@@ -0,0 +1,15 @@
+//Learn more about object (de structuring)
+
+// const course={
+//     name:"javascript learning",
+//     cost:2000,
+//     courseInstructor:"hitesh"
+// }
+
+// //Access values from object 
+// // console.log(course.name); // this is method to access from objects
+
+// const {name:courseName,cost,courseInstructor} = course; //this is latest method to print values from obejcts
+// console.log(courseName);
+
+
