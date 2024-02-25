9bd8f0ee64c34e31616b950aa535c5a95c62f0aa
shreesawant83 <81132129+shreesawant83@users.noreply.github.com>
2024-02-25 16:41:13 +0000

    learn about promises

---
+++ 09_API/promises.js
@@ -0,0 +1,77 @@
+//Learn about promises
+
+//promise is an object
+// const promiseOne = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         console.log("Promise One called");
+//         resolve();
+//     }, 2000)
+// })
+
+// promiseOne.then(function () {
+//     console.log("Promise worked");
+// })
+
+// new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         console.log("Set timeout promise");
+//         resolve();
+//     }, 2000)
+// }).then(function () {
+//     console.log("Promise called");
+// })
+
+
+// const promiseThree = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         console.log("Async Work");
+//         resolve({ username: "Shreepad83", pass: 123 });
+//     }, 2000)
+// });
+
+// promiseThree.then(function (userData) {
+// console.log(userData);
+// })
+
+// const promiseFour = new Promise((resolve, reject) => {
+//     setTimeout(function () {
+//         console.log("Set timeout");
+//         let Error = false;
+//         if (!Error) {
+//             resolve({ username: "Shreepad83", pass: 123 });
+//         } else {
+//             reject("Error : something went wrong");
+//         }
+//     })
+// })
+
+// promiseFour.then(function (users) {
+//     // console.log(users);
+//     return users.username;
+// }).catch(function (error) {
+//     console.log(error);
+// }).then(function (username) {
+//     console.log(username);
+// }).finally(function () {
+//     console.log("Promise either resolve or reject");
+// })
+
+
+// const promiseFive = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         console.log("Set time out called");
+//         // resolve({ username: "Shreepad83", pass: 123 })
+//         reject("Error happened")
+//     }, 2000)
+// })
+
+// async function getPromiseData() {
+//     try {
+//         const response = await promiseFive;
+//         console.log(response);
+//     } catch (error) {
+//         console.log(`${error} error log`);
+//     }
+// }
+
+// getPromiseData();
